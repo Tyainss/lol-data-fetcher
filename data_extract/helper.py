868efad31342ce55866be3c16f5e7743d98a9c44
Rui Pereira <ruiarpereira15@gmail.com>
2024-08-18 20:23:23 +0100

    :sparkles: Add merge_and_sum function to Helper class

--- data_extract/helper.py
+++ data_extract/helper.py
@@ -11,3 +11,11 @@ class Helper:
 
         date_str = datetime.fromtimestamp(epoch_date).strftime('%Y-%m-%d %H:%M:%S')
         return date_str
+    
+    def merge_and_sum(self, existing_df, new_df, key_columns, sum_columns):
+        if not existing_df.empty:
+            combined_df = pd.concat([existing_df, new_df])
+            combined_df = combined_df.groupby(key_columns, as_index=False)[sum_columns].sum()
+            return combined_df
+        else:
+            return new_df
